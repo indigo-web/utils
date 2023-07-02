eeef9de7d79446f579c010319e59bfd250e473ea
fakefloordiv <real.floordiv@gmail.com>
2023-07-02 16:45:05 +0200

    fix typo in imported package name

--- ft/sum.go
+++ ft/sum.go
@@ -6,6 +6,6 @@ func accumulator[T constraint.Addable](prev T, curr T) T {
 	return prev + curr
 }
 
-func Sum[T constraints.Addable](input []T) T {
+func Sum[T constraint.Addable](input []T) T {
 	return Reduce(accumulator[T], input)
 }
