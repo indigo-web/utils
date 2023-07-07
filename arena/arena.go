aae474f3a97a5a9625e21c9a53b60a5e78b6af48
fakefloordiv <real.floordiv@gmail.com>
2023-07-07 03:15:01 +0200

    made Arena generic & made API more convinient
    now Append receives not directly a slice, but variadic

--- arena/arena.go
+++ arena/arena.go
@@ -3,49 +3,51 @@ package arena
 // Arena is simply a big slice static-sized slice. It behaves just like built-in append()
 // function, except encapsulating its internal implementation, so no slice is being returned
 // to user - just boolean flag whether newly appended data exceeds size limits.
-type Arena struct {
-	memory     []byte
+type Arena[T any] struct {
+	memory     []T
 	begin, pos int
 
 	maxSize int
 }
 
-func NewArena(initialSpace, maxSpace int) *Arena {
-	return &Arena{
-		memory:  make([]byte, initialSpace),
+func NewArena[T any](initialSpace, maxSpace int) *Arena[T] {
+	return &Arena[T]{
+		memory:  make([]T, initialSpace),
 		maxSize: maxSpace,
 	}
 }
 
 // Append appends bytes to a buffer. In case of exceeding the maximal size, false is returned
 // and data isn't written
-func (a *Arena) Append(chars []byte) (ok bool) {
-	if a.pos+len(chars) > len(a.memory) {
-		if len(a.memory)+len(chars) >= a.maxSize {
+func (a *Arena[T]) Append(elements ...T) (ok bool) {
+	if a.pos+len(elements) > len(a.memory) {
+		if len(a.memory)+len(elements) >= a.maxSize {
 			return false
 		}
 
-		copy(a.memory[a.pos:], chars)
-		a.memory = append(a.memory, chars[len(a.memory)-a.pos:]...)
-		a.pos += len(chars)
+		copy(a.memory[a.pos:], elements)
+		// rely on the built-in slice growth mechanism. This sometimes exceeds the size limit
+		// without notifying the user anyhow, but this doesn't bother anybody at the moment
+		a.memory = append(a.memory, elements[len(a.memory)-a.pos:]...)
+		a.pos += len(elements)
 
 		return true
 	}
 
-	copy(a.memory[a.pos:], chars)
-	a.pos += len(chars)
+	copy(a.memory[a.pos:], elements)
+	a.pos += len(elements)
 
 	return true
 }
 
-func (a *Arena) Finish() []byte {
+func (a *Arena[T]) Finish() []T {
 	segment := a.memory[a.begin:a.pos]
 	a.begin = a.pos
 
 	return segment
 }
 
-func (a *Arena) Clear() {
+func (a *Arena[T]) Clear() {
 	a.begin = 0
 	a.pos = 0
 }
--- arena/arena_test.go
+++ arena/arena_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func pushSegment(t *testing.T, arena *Arena, text string) {
-	ok := arena.Append([]byte(text))
+func pushSegment(t *testing.T, arena *Arena[byte], text string) {
+	ok := arena.Append([]byte(text)...)
 	require.True(t, ok)
 	segment := arena.Finish()
 	require.Equal(t, text, string(segment))
@@ -14,13 +14,13 @@ func pushSegment(t *testing.T, arena *Arena, text string) {
 
 func TestArena(t *testing.T) {
 	t.Run("NoOverflow", func(t *testing.T) {
-		arena := NewArena(10, 20)
+		arena := NewArena[byte](10, 20)
 		pushSegment(t, arena, "Hello")
 		pushSegment(t, arena, "Here")
 	})
 
 	t.Run("YesOverflow", func(t *testing.T) {
-		arena := NewArena(10, 20)
+		arena := NewArena[byte](10, 20)
 		// "Hello, World!" is 13 characters length, so it will force the Arena
 		// to grow an underlying slice
 		pushSegment(t, arena, "Hello, ")
@@ -28,12 +28,12 @@ func TestArena(t *testing.T) {
 	})
 
 	t.Run("SizeLimitOverflow", func(t *testing.T) {
-		arena := NewArena(10, 20)
+		arena := NewArena[byte](10, 20)
 		pushSegment(t, arena, "Hello, ")
 		pushSegment(t, arena, "World!")
 		pushSegment(t, arena, "Lorem ")
 		// at this point, we have reached 19 elements in underlying slice
-		ok := arena.Append([]byte("overflow"))
+		ok := arena.Append([]byte("overflow")...)
 		require.False(t, ok)
 	})
 }
