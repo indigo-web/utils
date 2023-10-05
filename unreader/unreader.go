86103e48e9a833c03f55560b6318c7febdd5d9c4
fakefloordiv <real.floordiv@gmail.com>
2023-10-06 01:33:26 +0200

    made pending data public, added docstrings

--- unreader/unreader.go
+++ unreader/unreader.go
@@ -1,22 +1,28 @@
 package unreader
 
+// Unreader allows to store extra bytes, and return them on the next call.
+//
+// It's ready to work right after the instantiation.
 type Unreader struct {
-	pending []byte
+	Pending []byte
 }
 
-func (u *Unreader) PendingOr(or func() ([]byte, error)) (data []byte, err error) {
-	if len(u.pending) > 0 {
-		data, u.pending = u.pending, nil
+// PendingOr returns pending data if available, otherwise returns the result of the passed function
+func (u *Unreader) PendingOr(otherwise func() ([]byte, error)) (data []byte, err error) {
+	if len(u.Pending) > 0 {
+		data, u.Pending = u.Pending, nil
 		return data, nil
 	}
 
-	return or()
+	return otherwise()
 }
 
+// Unread saves extra, that'll be returned on the next call
 func (u *Unreader) Unread(b []byte) {
-	u.pending = b
+	u.Pending = b
 }
 
+// Reset discards all the pending data
 func (u *Unreader) Reset() {
-	u.pending = nil
+	u.Pending = nil
 }
