ecc4e366e96b7eb4b5da7f390aff3eb202bf85c0
fakefloordiv <real.floordiv@gmail.com>
2023-12-11 16:17:13 +0100

    added tests for b2s

--- uf/b2s_test.go
+++ uf/b2s_test.go
@@ -1,1 +1,11 @@
 package uf
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestB2S(t *testing.T) {
+	bytes := []byte("Hello, world!")
+	require.Equal(t, "Hello, world!", B2S(bytes))
+}
