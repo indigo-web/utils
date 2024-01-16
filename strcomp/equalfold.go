06770188f59a5cca19a1a6ddc68902be957de615
fakefloordiv <real.floordiv@gmail.com>
2024-01-16 21:31:43 +0100

    roll back without loop unrolling
    suddenly it made the performance only worse

--- strcomp/equalfold.go
+++ strcomp/equalfold.go
@@ -5,40 +5,11 @@ func EqualFold(a, b string) bool {
 		return false
 	}
 
-	var i int
-
-	for ; i < len(a)-8; i += 8 {
-		if a[i]|0x20 != b[i]|0x20 {
-			return false
-		}
-		if a[i+1]|0x20 != b[i+1]|0x20 {
-			return false
-		}
-		if a[i+2]|0x20 != b[i+2]|0x20 {
-			return false
-		}
-		if a[i+3]|0x20 != b[i+3]|0x20 {
-			return false
-		}
-		if a[i+4]|0x20 != b[i+4]|0x20 {
-			return false
-		}
-		if a[i+5]|0x20 != b[i+5]|0x20 {
-			return false
-		}
-		if a[i+6]|0x20 != b[i+6]|0x20 {
-			return false
-		}
-		if a[i+7]|0x20 != b[i+7]|0x20 {
-			return false
-		}
-	}
-
-	for ; i < len(a); i++ {
+	for i := range a {
 		if a[i]|0x20 != b[i]|0x20 {
 			return false
 		}
 	}
-
+	
 	return true
 }
